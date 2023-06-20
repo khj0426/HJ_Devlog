af4fd9c6922cc88b861a3fc2a34380e9c2b95c5e
Kim0426 <706shin1728@naver.com>
2023-06-20 17:18:09 +0900

    chore:rehype-raw 패키지 추가

--- lib/markdowntoHTML.ts
+++ lib/markdowntoHTML.ts
@@ -2,7 +2,9 @@ import { remark } from 'remark';
 import html from 'remark-html';
 
 export default async function markdownToHtml(markdown: string) {
-  const result = await remark().use(html).process(markdown);
+  const result = await remark().use(html,{
+    sanitize:false
+  }).process(markdown);
 
   return result.toString();
 }
