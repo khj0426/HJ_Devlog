10e95f10356ff7afb1917d8372f47d315d21682c
Kim0426 <706shin1728@naver.com>
2023-07-01 22:14:32 +0900

    feat:번들 크기 측정하게 프로젝트 설정
    next-bundle-analyzer라이브러리 사용

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
+
 const nextConfig = {
   reactStrictMode: true,
   compiler: {
@@ -6,4 +10,7 @@ const nextConfig = {
   },
 };
 
-module.exports = nextConfig;
+module.exports = withBundleAnalyzer({
+  nextConfig,
+  compress: true,
+});
