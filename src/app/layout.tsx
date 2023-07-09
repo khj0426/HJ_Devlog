89574890ee0ce7748803303f73e778b06d63298f
Kim0426 <706shin1728@naver.com>
2023-07-09 20:26:20 +0900

    refactor:openGraph수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,21 +4,20 @@ import GlobalStyle from '@/style/globalStyle';
 import { ThemeWrapper } from './themeWrapper';
 import Navbar from '@/Component/Common/Navbar';
 import Footer from '@/Component/Common/Footer';
-import Head from 'next/head';
 import localFont from 'next/font/local';
-
+import { Metadata } from 'next';
 
 const baseFont = localFont({
-  src:'./fonts/KyoboHandwriting2022khn.ttf',
-  display:'swap'
-})
-export const metadata = {
+  src: './fonts/KyoboHandwriting2022khn.ttf',
+  display: 'swap',
+});
+export const metadata: Metadata = {
   title: 'HJ`s Blog',
   description: '개발관련 여러 지식을 기록하는 공간',
   icons: {
     icon: '/images/favicon.png',
   },
-  url: 'https://hj-devlog.vercel.app',
+  metadataBase: new URL('https://hj-devlog.vercel.app/'),
   openGraph: {
     url: 'https://hj-devlog.vercel.app',
     title: 'HJ`s Blog',
@@ -29,8 +28,26 @@ export const metadata = {
         width: 800,
         height: 600,
       },
+      {
+        url: 'https://avatars.githubusercontent.com/u/59411107?v=4',
+        width: 1800,
+        height: 1600,
+        alt: 'My custom alt',
+      },
     ],
     type: 'website',
+  },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+    },
+  },
+  viewport: {
+    width: 'device-width',
+    initialScale: 1,
   },
 };
 
@@ -40,27 +57,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className = {baseFont.className}>
-      <Head>
-        <meta
-          name="google-site-verification"
-          content="g3Daim29whdK1ZzL1CE6pvkYyvSgM5-6C898-TVjiz0"
-        />
-        <meta
-          property="og:image"
-          content="https://avatars.githubusercontent.com/u/59411107?v=4"
-        />
-
-        <meta property="og:url" content="https://avatars.githubusercontent.com/u/59411107?v=4" />
-
-        <meta property="og:title" content="효중킴의 개발 블로그" />
-        <meta
-          property="og:description"
-          content="개발 관련 여러 정보를 정리하고 기록하는 공간입니다."
-        />
-        <meta property="og:image:width" content="1200" />
-        <meta property="og:image:height" content="630" />
-      </Head>
+    <html lang="en" className={baseFont.className}>
       <body className="App">
         <Recoil>
           <ThemeWrapper>
