8702d4a6f3b6400681a66bddff7b86008376e548
Kim0426 <706shin1728@naver.com>
2023-07-14 01:35:06 +0900

    feat:글의 카테고리 버튼 랜더링하게 설정
    추가적인 스타일링 + 경로 생성이 필요한 상태!

--- lib/api.ts
+++ lib/api.ts
@@ -44,7 +44,37 @@ export function getAllPosts(fields: string[] = []) {
   const posts = slugs
     .map((slug) => getPostBySlug(slug, fields))
     // sort posts by date in descending order
-
     .sort((post1, post2) => (post1.date > post2.date ? -1 : 1));
+
+  return posts;
+}
+
+export function getAllCategories() {
+  const allPosts = getAllPosts(['category']);
+
+  const allCategory = new Map<string, number>();
+  allPosts.map((post) => {
+    if (allCategory.has(post.category)) {
+      const getCategoryCount = allCategory.get(post.category) || 0;
+      allCategory.set(post.category, getCategoryCount + 1);
+    } else {
+      allCategory.set(post.category, 1);
+    }
+  });
+
+  return Array.from(allCategory);
+}
+
+export function getCategoryFilteredPosts(
+  fields: string[] = [],
+  category: string
+) {
+  const slugs = getPostSlugs();
+  const posts = slugs
+    .map((slug) => getPostBySlug(slug, fields))
+    // sort posts by date in descending order
+    .sort((post1, post2) => (post1.date > post2.date ? -1 : 1))
+    .filter((post) => post.category === category);
+
   return posts;
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,44 @@
-import { getAllPosts } from '../../lib/api';
+import {
+  getAllPosts,
+  getAllCategories,
+  getCategoryFilteredPosts,
+} from '../../lib/api';
 import Link from 'next/link';
 import Title from '@/Component/About/Title';
 import PostLayout from '@/Component/Common/PostLayout';
-import PostExterct from '@/Component/Blog/Exterct';
+import Button from '@/Component/Common/Button';
 
 export default function Home() {
-  const posts = getAllPosts(['title', 'data', 'slug', 'category', 'excerpt','date']);
+  const posts = getAllPosts([
+    'title',
+    'data',
+    'slug',
+    'category',
+    'excerpt',
+    'date',
+  ]);
 
-  posts.sort((post1,post2) => post1.date > post2.date ? -1 : 1);
+  posts.sort((post1, post2) => (post1.date > post2.date ? -1 : 1));
 
+  const allCategory = getAllCategories();
   return (
-    <main>
-      {posts.map((post) => (
-        <PostLayout key={post.title}>
-          <Link href={`/blog/${post.slug}`}>
-            <Title title={post.title} />
-            <p>{post.category}</p>
-            {post.content}
-          </Link>
-        </PostLayout>
-      ))}
-    </main>
+    <>
+      <nav>
+        {Array.from(allCategory).map(([category, categoryCount]) => (
+          <div key={category}>{category + categoryCount}</div>
+        ))}
+      </nav>
+      <main>
+        {posts.map((post) => (
+          <PostLayout key={post.title}>
+            <Link href={`/blog/${post.slug}`}>
+              <Title title={post.title} />
+              <p>{post.category}</p>
+              {post.content}
+            </Link>
+          </PostLayout>
+        ))}
+      </main>
+    </>
   );
 }
