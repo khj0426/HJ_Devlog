b33cd49bdd9f5bdf7350ae8247cea62b67f3752c
Kim0426 <706shin1728@naver.com>
2023-08-02 14:03:05 +0900

    refactor:디바운스 시간 변경

--- src/hooks/useSearchPost.tsx
+++ src/hooks/useSearchPost.tsx
@@ -8,6 +8,10 @@ export default function useSearchPost(searchInput: string) {
   const [posts, setPosts] = useState<Item[]>([]);
 
   useEffect(() => {
+    const debounceHandler = setTimeout(() => {
+      fetchAllPosts();
+    }, 1000);
+
     const fetchAllPosts = async () => {
       const allPostResponse = await fetch('/api/slugs');
       const allPosts: Item[] = await allPostResponse.json();
@@ -15,7 +19,10 @@ export default function useSearchPost(searchInput: string) {
         setPosts(allPosts.filter((post) => post.title.includes(searchInput)));
       }
     };
-    fetchAllPosts();
+
+    return () => {
+      clearTimeout(debounceHandler);
+    };
   }, [searchInput]);
 
   return {
