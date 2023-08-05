a9cf1594ecc4eb89f7e45c3fac360a740e7ee64b
Kim0426 <706shin1728@naver.com>
2023-08-05 22:12:46 +0900

    refactor:lighthouse-ci 코드에서 ci의 upload옵션 수정

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,35 +1,29 @@
 const { basename, join } = require('path');
 const fs = require('fs');
 
-const makeUrls = () => {
-  const localServerPrefix = 'http://localhost:3000/';
+const makeURLS = () => {
+  const Prefix = 'https://hj-devlog.vercel.app/';
 
   const postPath = join(process.cwd(), 'posts');
   const postSlug = fs.readdirSync(postPath);
   const allPostPath = postSlug.map((post) => {
-    return (
-      localServerPrefix + 'blog/' + encodeURIComponent(post.replace('.md', ''))
-    );
+    return Prefix + 'blog/' + encodeURIComponent(post.replace('.md', ''));
   });
 
   return {
-    allPostPaths: [
-      ...allPostPath,
-      localServerPrefix,
-      localServerPrefix + 'about',
-    ],
+    allPostPaths: [...allPostPath, Prefix, Prefix + 'about'],
   };
 };
 
 module.exports = {
   ci: {
     collect: {
-      startServerCommand: 'npm run dev',
-      url: makeUrls().allPostPaths,
+      startServerCommand: 'npm run start',
+      url: makeURLS().allPostPaths,
     },
     upload: {
-      target: 'filesystem',
-      outputDir: './lhci_reports',
+      target: 'temporary-public-storage',
+      outputDir: '/lhci_reports',
     },
   },
 };
