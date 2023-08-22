29347f2ab88cefb5150e2d92a2a5f2e761334db7
Kim0426 <706shin1728@naver.com>
2023-08-23 02:06:04 +0900

    chore:sentry/utils 패키지 개발 의존성 추가

--- next.config.js
+++ next.config.js
@@ -1,4 +1,3 @@
-/** @type {import('next').NextConfig} */
 const { withSentryConfig } = require('@sentry/nextjs');
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
@@ -9,11 +8,6 @@ const nextConfig = {
   compiler: {
     styledComponents: true,
   },
-};
-
-module.exports = withBundleAnalyzer({
-  nextConfig,
-  compress: true,
   images: {
     remotePatterns: [
       {
@@ -23,38 +17,22 @@ module.exports = withBundleAnalyzer({
     ],
     formats: ['image/avif', 'image/webp'],
   },
+};
+
+const bundleAnalyzerConfig = withBundleAnalyzer({
+  ...nextConfig,
+  compress: true,
 });
 
-// Injected content via Sentry wizard below
+const sentryConfig = {
+  silent: true,
+  org: 'kim0426-00d4b8e8c',
+  project: 'my-blog',
+  widenClientFileUpload: true,
+  transpileClientSDK: true,
+  tunnelRoute: '/monitoring',
+  hideSourceMaps: true,
+  disableLogger: true,
+};
 
-module.exports = withSentryConfig(
-  module.exports,
-  {
-    // For all available options, see:
-    // https://github.com/getsentry/sentry-webpack-plugin#options
-
-    // Suppresses source map uploading logs during build
-    silent: true,
-    org: 'kim0426-00d4b8e8c',
-    project: 'my-blog',
-  },
-  {
-    // For all available options, see:
-    // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
-
-    // Upload a larger set of source maps for prettier stack traces (increases build time)
-    widenClientFileUpload: true,
-
-    // Transpiles SDK to be compatible with IE11 (increases bundle size)
-    transpileClientSDK: true,
-
-    // Routes browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers (increases server load)
-    tunnelRoute: '/monitoring',
-
-    // Hides source maps from generated client bundles
-    hideSourceMaps: true,
-
-    // Automatically tree-shake Sentry logger statements to reduce bundle size
-    disableLogger: true,
-  }
-);
+module.exports = withSentryConfig(bundleAnalyzerConfig, sentryConfig);
