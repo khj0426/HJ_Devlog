4f433f7575dc6a0613e7ed4e28a3e4a354fd456c
Kim0426 <706shin1728@naver.com>
2023-12-18 15:20:36 +0900

    refactor: 빌드 오류 수정

--- src/Component/Notion/NotionresumeClient.tsx
+++ src/Component/Notion/NotionresumeClient.tsx
@@ -31,7 +31,6 @@ export default function ResumeClient({
         components={{
           Collection,
           Modal,
-          nextImage: Image,
         }}
         recordMap={recordMap}
         fullPage={true}
