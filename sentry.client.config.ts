02b6cb8c79738d5329a08c570f64bb048f16f7a0
Kim0426 <706shin1728@naver.com>
2023-12-21 16:37:30 +0900

    chore: production 모드에서만 sentry 적용

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -2,23 +2,31 @@
 // The config you add here will be used whenever a users loads a page in their browser.
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
-import { init } from "@sentry/nextjs";
+import { init, Replay } from '@sentry/nextjs';
 
-init({
-  dsn: "https://4b6a5c9b4d17bd0e5e3b8913c66c9a34@o4505749556363264.ingest.sentry.io/4505749558591488",
+if (process.env.NODE_ENV === 'production') {
+  init({
+    dsn: 'https://4b6a5c9b4d17bd0e5e3b8913c66c9a34@o4505749556363264.ingest.sentry.io/4505749558591488',
 
-  // Adjust this value in production, or use tracesSampler for greater control
-  tracesSampleRate: 1,
+    // Adjust this value in production, or use tracesSampler for greater control
+    tracesSampleRate: 1,
 
-  // Setting this option to true will print useful information to the console while you're setting up Sentry.
-  debug: false,
+    // Setting this option to true will print useful information to the console while you're setting up Sentry.
+    debug: false,
 
-  replaysOnErrorSampleRate: 1.0,
+    replaysOnErrorSampleRate: 1.0,
 
-  // This sets the sample rate to be 10%. You may want this to be 100% while
-  // in development and sample at a lower rate in production
-  replaysSessionSampleRate: 0.1,
+    // This sets the sample rate to be 10%. You may want this to be 100% while
+    // in development and sample at a lower rate in production
+    replaysSessionSampleRate: 0.1,
 
-  // You can remove this option if you're not planning to use the Sentry Session Replay feature:
- 
-});
+    // You can remove this option if you're not planning to use the Sentry Session Replay feature:
+    integrations: [
+      new Replay({
+        // Additional SDK configuration goes in here, for example:
+        maskAllText: true,
+        blockAllMedia: true,
+      }),
+    ],
+  });
+}
