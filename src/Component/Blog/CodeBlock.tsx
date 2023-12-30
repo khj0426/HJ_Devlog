d370181512ca0e289cd4172e03aefce022927535
Kim0426 <706shin1728@naver.com>
2023-12-30 16:24:10 +0900

    refactor: 코드블록 글꼴 수정

--- src/Component/Blog/CodeBlock.tsx
+++ src/Component/Blog/CodeBlock.tsx
@@ -15,6 +15,7 @@ export default function CodeBlock({
       language="javascript"
       style={materialDark}
       customStyle={{
+        fontFamily: '__Do_Hyeon_7b3cf7',
         maxWidth: '100vw',
         wordBreak: 'break-all',
         overflowWrap: 'break-word', // 추가: 화면 넘어갈 때 단어 단위로 줄바꿈 처리
