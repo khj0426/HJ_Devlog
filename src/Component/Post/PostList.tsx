68c046019e118c57b57bebb109bfcd4a34158d91
Kim0426 <706shin1728@naver.com>
2024-01-01 17:42:02 +0900

    refactor: 새로운 글 목록 가져올 때 스피너 추가

--- src/Component/Post/PostList.tsx
+++ src/Component/Post/PostList.tsx
@@ -1,12 +1,13 @@
 'use client';
 
+import Spinner from '@/Component/Common/Spinner';
 import usePostQuery from '@/hooks/queries/usePostQuery';
 import useObserver from '@/hooks/useObserver';
 
 import PostItem from './PostItem';
 
 export default function PostServiceLayer() {
-  const { data, fetchNextPage, hasNextPage } = usePostQuery();
+  const { data, fetchNextPage, hasNextPage, isFetching } = usePostQuery();
   const { target } = useObserver({
     threshold: 0.1,
     hasNextPage,
@@ -17,6 +18,16 @@ export default function PostServiceLayer() {
     <>
       {data?.pages?.map((page) =>
         page.posts.map((post) => <PostItem post={post} key={post.title} />)
+      )}
+      {isFetching && (
+        <div
+          style={{
+            display: 'flex',
+            justifyContent: 'center',
+          }}
+        >
+          <Spinner size={50} />
+        </div>
       )}
       <div ref={target}></div>
     </>
