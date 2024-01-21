da4f504717382f28ae6562e4b75db0028a729e7e
Kim0426 <706shin1728@naver.com>
2024-01-21 14:07:55 +0900

    refactor: ga 문법 오류 수정

--- src/Component/GA/GA.tsx
+++ src/Component/GA/GA.tsx
@@ -15,8 +15,7 @@ const GoogleAnalytics = () => {
         window.dataLayer = window.dataLayer || [];
         function gtag(){dataLayer.push(arguments);}
         gtag('js', new Date());
-
-        gtag('config', ${process.env.NEXT_PUBLIC_MEASUREMENT_ID});
+        gtag('config', '${process.env.NEXT_PUBLIC_MEASUREMENT_ID}');
         `,
         }}
       />
