319715be6d03e5c47bf142e5c1ab69a5496ad5a8
Kim0426 <706shin1728@naver.com>
2024-02-21 01:47:56 +0900

    refactor: use server 구문 제거

--- src/services/GuestBook/index.ts
+++ src/services/GuestBook/index.ts
@@ -3,7 +3,5 @@ import type { GuestBook } from '@/@types/GuestBookType';
 import { get } from '@/utils/axiosClient';
 
 export const getGuestBook = async () => {
-  'use server';
-
   return (await get<GuestBook>('/api/guestbook')).data;
 };
