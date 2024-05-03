3ab64c9cc637f8133dd1bf47a0657d8e98d0ded3
Kim0426 <706shin1728@naver.com>
2024-05-03 11:19:54 +0900

    refactor: 다크모드 시 추천 글 모달 색상 변경

--- src/Component/Blog/RecommendPostModal/RecommendPostModal.tsx
+++ src/Component/Blog/RecommendPostModal/RecommendPostModal.tsx
@@ -6,6 +6,7 @@ import { useEffect } from 'react';
 
 import Link from 'next/link';
 import { useRecoilState } from 'recoil';
+import styled from 'styled-components';
 
 import { postRecommendModalClose } from '@/app/Providers/Recoil/globalAtom';
 import Modal from '@/Component/Common/Modal/Modal';
@@ -17,6 +18,10 @@ import PostItem from '@/Component/Post/PostItem';
 import useIntersectionObserver from '@/hooks/useIntersectionObserver';
 import useModal from '@/hooks/useModal';
 
+const StyledRecommendPostModalWrapper = styled.div`
+  color: ${({ theme }) => theme.currentTheme.text};
+  background-color: ${({ theme }) => theme.currentTheme.backgroundPost};
+`;
 export default function RecommendPostModal({
   randomPosts,
 }: {
@@ -34,43 +39,64 @@ export default function RecommendPostModal({
   return (
     <>
       {!close && (
-        <Modal id={modal.id} disabledPortal>
-          <ModalContent
-            closeOutSideClick={closeModal}
-            width={'300px'}
-            height={'400px'}
-          >
-            <ModalHeader as="h4">🥰읽어주셔서 감사합니다</ModalHeader>
-            🧡이런글은 어떠신가요?
-            <div
-              style={{
-                display: 'flex',
-                flexDirection: 'column',
-                fontSize: '16px',
-                justifyContent: 'space-between',
-              }}
+        <StyledRecommendPostModalWrapper>
+          <Modal id={modal.id} disabledPortal>
+            <ModalContent
+              closeOutSideClick={closeModal}
+              width={'300px'}
+              height={'400px'}
             >
-              {randomPosts.map((post) => {
-                return (
-                  <Link key={post.title} href={`/blog/${post.slug}`}>
-                    {post.title}
-                  </Link>
-                );
-              })}
-            </div>
-            <ModalFooter>
-              <ModalCloseButton
-                onClick={() => {
-                  closeModal();
-                  setClose(true);
-                }}
+              <ModalHeader
+                as="h4"
                 style={{
-                  background: '#ededed',
+                  color: '#495057',
                 }}
-              />
-            </ModalFooter>
-          </ModalContent>
-        </Modal>
+              >
+                🥰읽어주셔서 감사합니다
+              </ModalHeader>
+              <p
+                style={{
+                  color: '#495057',
+                }}
+              >
+                🧡이런글은 어떠신가요?
+              </p>
+              <div
+                style={{
+                  display: 'flex',
+                  flexDirection: 'column',
+                  fontSize: '16px',
+                  justifyContent: 'space-between',
+                }}
+              >
+                {randomPosts.map((post) => {
+                  return (
+                    <Link
+                      key={post.title}
+                      href={`/blog/${post.slug}`}
+                      style={{
+                        color: '#495057',
+                      }}
+                    >
+                      {post.title}
+                    </Link>
+                  );
+                })}
+              </div>
+              <ModalFooter>
+                <ModalCloseButton
+                  onClick={() => {
+                    closeModal();
+                    setClose(true);
+                  }}
+                  style={{
+                    background: '#ededed',
+                  }}
+                />
+              </ModalFooter>
+            </ModalContent>
+          </Modal>
+        </StyledRecommendPostModalWrapper>
       )}
 
       <div ref={target}></div>
