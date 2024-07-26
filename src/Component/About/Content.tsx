cfef45f3dd71144b6b5a26d560c50280831763f4
Kim0426 <706shin1728@naver.com>
2024-07-26 11:28:06 +0900

    refactor: about페이지 패딩 수정

--- src/Component/About/Content.tsx
+++ src/Component/About/Content.tsx
@@ -1,21 +1,15 @@
 export default function Content({ content }: { content: string | string[] }) {
-  return (
-    <section
-      style={{
-        display: 'flex',
-        minWidth: '300px',
-        minHeight: '400px',
-        maxWidth: '400px',
-        maxHeight: '500px',
-        fontSize: '18px',
-        flexDirection: 'column',
-      }}
-    >
-      {Array.isArray(content) ? (
-        content.map((eachContent) => <p key={eachContent}>{eachContent}</p>)
-      ) : (
-        <p key={content}>{content}</p>
-      )}
-    </section>
-  );
+    return (
+        <section
+            style={{
+                display: 'flex',
+                minWidth: '300px',
+                maxWidth: '400px',
+                fontSize: '15px',
+                flexDirection: 'column',
+            }}
+        >
+            {Array.isArray(content) ? content.map((eachContent) => <p key={eachContent}>{eachContent}</p>) : <p key={content}>{content}</p>}
+        </section>
+    )
 }
